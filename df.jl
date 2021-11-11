1ff31ea9822721f1df36df45ae5123691ffc01cf
gabrielsg <gabriel.c.rodrigues@gmail.com>
2021-11-11 21:11:10 +0800

    indexing

--- df.jl
+++ df.jl
@@ -68,3 +68,52 @@ function names_grades1()
 end
 
 names_grades1()
+
+
+function names_grades2()
+    df = grades_2020()
+    df[!, :name] # ! in-place modification
+end
+
+names_grades2()
+
+
+function grade_2020(i::Int)
+    df = grades_2020()
+    df[i, :]
+end
+
+grade_2020(2)
+
+
+grades_indexing(df::DataFrame) = df[1:2, :name]
+grades_indexing(grades_2020())
+
+function grade_2020(name::String)
+    df = grades_2020()
+    dic = Dict(zip(df.name, df.grade_2020))
+    dic[name]
+end
+
+grade_2020("Bob")
+
+# Filter function
+equals_alice(name::String) = name == "Alice"
+equals_alice("Bob")
+
+filter(:name => equals_alice, grades_2020())
+filter(equals_alice, ["Alice", "Bob", "James"]) # besides DataFrame, also works with vectors
+
+filter(n -> n == "Alice", ["Alice", "Bob", "James"]) # annonymous function more concise
+
+filter(:name => n -> n == "Alice", grades_2020())
+filter(:name => ==("Alice"), grades_2020()) # even more concise; note no space after ==
+filter(:name => !=("Alice"), grades_2020())
+
+function complex_filter(name, grade)::Bool
+    interesting_name = startswith(name, 'A') || startswith(name, 'B')
+    interesting_grade = grade > 4
+    interesting_name && interesting_grade
+end
+
+filter([:name, :grade_2020] => complex_filter, grades_2020())
